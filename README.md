d6eb08fb62987b4dd33acbf050d4e7c145d48d59
wiame taii <wiametaii@etu.univ-nantes.fr>
2022-03-24 11:51:54 +0100

    add

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 
+Dans ce projet on aura pour but de creer un jeu de carte qui consiste creer des cartes et les manipuler ; il y a cinquantes deux carte dans un paquet dant chcune appartient a une des quatre couleur et l'une des treize valeurs . Les couleur sont pique , coeur , carreau et trefle .
+ce peojet consiste a tester les competence qu'on a acquis au cours de nos seance de tp 
+Pour ce peojet on aura besoin d'un module carte, qui permet de créer des cartes et de les manipuler. Ce module doit comprendre 
+Une enumération Couleur qui définit les différentes couleurs d’un jeu de carte (pique, carreau, trèfle et cœur).
+Un enregistrement Carte qui permet de représenter des cartes, chacune avec une couleur et une valeur allant de 2 à 14 .
+Une fonction creerCarte pour créer une carte à partir d’un couleur et d’une valeur. on aura besoin aussi d'une  fonction afficherCarte qui affiche une carte dans la console et ensuite Une fonction comparerCartes qui prend deux cartes et renvoie une valeur positive si la première carte est plus forte, zéro si elles ont les mêmes valeurs, et une valeur négative si la première carte est moins forte.
+Dernierement Un module jeu qui permet de créer et manipuler un jeu de cartes
+D'abord une fonction creerJeu qui renvoie l’ensemble des 52 cartes possibles sous la forme d’un tableau , et finalement une fonction tirerCarte qui retire la première carte d’un tableau de cartes, et qui la rend en valeur de retour. 
+pour effectuer ce projet on aura de git lab qui est efficace en terme de projet C’est un outil particulièrement pratique pour éviter les pertes de données, et pour travailler à plusieurs sur un même projet . 
+La structure du projet sera sous la forme d'un repertoire src qui va implementer tous les elments expliquer en haut on aura besoin d'un fichier  tsconfig.json Un fichier  package.json
+Un fichier .gitignore contenant la liste de tous les répertoires qui seront générés .
+
--- src/module_carte.ts
+++ src/module_carte.ts
@@ -17,23 +17,31 @@ type Carte = {
 //Une fonction creerCarte pour créer une carte à partir d’un couleur et d’une valeur.
 export function creerCarte(couleur: Couleur, valeur: number): Carte {
       let carte: Carte
+    // @carte.couleur  pour definir la couleur d'une carte
       carte.couleur = couleur
+      //@carte.valeur pour definir la valeur d'une carte 
       carte.valeur = valeur
 
       return carte
 };
-//--------------------------------------
+//-------------------------------------
 //Une fonction afficherCarte qui affiche une carte 
 export function AfficheCarte(C: Carte) {
+      // affiche la valeur d'une carte
       console.log("valeur de la carte :" + C.valeur)
+      // affiche la couleur d'une carte 
       console.log("couleur de la carte :" + C.couleur)
 
 };
 
 //------------------------------------------------
-// une fonction qui compare entre deux cartes
+ //une fonction qui compare entre deux cartes et renvoie une valeur positive si la première carte est plus forte, 
+//zéro si elles ont les mêmes valeurs, et une valeur négative si la première carte est moins forte.
+
 export function compareCarte(self: Carte, other: Carte): boolean {
       // varifie les couleurs 
+      //@ self.couleur c'est  une premiere carte
+      //@other.couleur c'est une deuxieme carte
       if (self.couleur < other.couleur)
             return false
       else
@@ -42,27 +50,31 @@ export function compareCarte(self: Carte, other: Carte): boolean {
       return self.valeur > other.valeur
 };
 //--------------------------------------------
+// Une fonction creerJeu qui renvoie l’ensemble des 52 cartes possibles sous la forme d’un tableau.
 export function creerJeu(): Array<Carte> {
       let jeuxComplet: Array<Carte>
       let carte: Carte
-
+      // une boucle pour qui va de 2 a 14 et qui defini la couleur et la valeur des cartes
       for (let i = 2; i <= 14; i++) {
             carte.couleur = Couleur.coeur
             carte.valeur = i
             jeuxComplet.push(carte)
 
-      };
+      }
+      //Une fonction creerJeu qui renvoie l’ensemble des 52 cartes possibles sous la forme d’un tableau.
       for (let i = 2; i <= 14; i++) {
             carte.couleur = Couleur.carreau
             carte.valeur = i
             jeuxComplet.push(carte)
       };
+       //Une fonction creerJeu qui renvoie l’ensemble des 52 cartes possibles sous la forme d’un tableau.
       for (let i = 2; i <= 14; i++) {
             carte.couleur = Couleur.trefle
             carte.valeur = i
             jeuxComplet.push(carte)
                   ;
       }
+       //Une fonction creerJeu qui renvoie l’ensemble des 52 cartes possibles sous la forme d’un tableau.
       for (let i = 2; i <= 14; i++) {
             carte.couleur = Couleur.pique
             carte.valeur = i
@@ -74,9 +86,10 @@ export function creerJeu(): Array<Carte> {
 }
 
 //-----------------------------------
+//Une fonction tirerCarte qui retire la première carte d’un tableau de cartes, et qui la rend en valeur de retour.
  export function tireCarte (  t_carte: Array <Carte> ): Carte {
-      let Carte : Carte 
-      Carte = t_carte[0] 
+      let Carte : Carte // on definit une carte 
+      Carte = t_carte[0] // qui prend pour indice 0
       t_carte.splice (0, 1)
       return Carte 
  }
